e0f2ed655c084fa8814543b1287b43a9895da18d
duncan <duncan2148@icloud.com>
2021-02-16 11:57:05 +0000

    current commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 //test
 const http = require("http");
-
+const express = require("express");
+const app = express();
+app.set("view-engine", "ejs");
 const hostname = "127.0.0.1";
 const port = process.env.PORT || 3000;
 
@@ -9,6 +11,7 @@ const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello World");
 });
+
 
 server.listen(port, hostname, () => {
   console.log(`Server running at port: ${port}/`);
